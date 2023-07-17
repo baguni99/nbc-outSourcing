538a9f8f34ea8c5293ffafc0d0bfbe330663bd73
yui62yui <you@example.com>
2023-07-17 13:31:05 +0900

    페이지 html, css 기초틀 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Sub2 from "./pages/Sub2";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Sub2></Sub2>
+    </div>
+  );
 }
 
 export default App;
