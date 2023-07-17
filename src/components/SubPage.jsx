203079a60913f3b5a5072d69356629a1ba514afa
eunhee <eunhee9902@gmail.com>
2023-07-18 01:09:00 +0900

    react-query활용 유튜브 영상 50개 불러오기

--- src/components/SubPage.jsx
+++ src/components/SubPage.jsx
@@ -1,48 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import axios from 'axios';
 
-const API_URL =
-  'https://www.googleapis.com/youtube/v3/search?part=snippet&q=%EC%9E%90%EC%B7%A8%EC%83%9D%20%EB%A0%88%EC%8B%9C%ED%94%BC&key=AIzaSyA5LqbiURh00hMQd_nspmkQ0BMtgpmJKdw';
-const API_KEY = 'AIzaSyA5LqbiURh00hMQd_nspmkQ0BMtgpmJKdw';
-
-const SubPage = () => {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    fetchCategoryVideos('자취생 레시피');
-  }, []);
-
-  const fetchCategoryVideos = async (category) => {
-    try {
-      const response = await axios.get(API_URL, {
-        params: {
-          part: 'snippet',
-          q: category,
-          key: API_KEY
-        }
-      });
-
-      const videos = response.data.items;
-      setVideos(videos);
-    } catch (error) {
-      console.log(error);
+export const fetchVideos = async (category) => {
+  const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+    params: {
+      part: 'snippet',
+      q: category,
+      key: 'AIzaSyA5LqbiURh00hMQd_nspmkQ0BMtgpmJKdw',
+      maxResults: 50
     }
-  };
+  });
 
-  return (
-    <div>
-      <h1>자취방에서 살아남기</h1>
-      <h2>자취생 레시피</h2>
-      {videos.map((video) => (
-        <div key={video.id.videoId}>
-          <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
-          <h3>{video.snippet.title}</h3>
-          <p>{video.snippet.publishedAt}</p>
-          <p>{video.snippet.channelTitle}</p>
-        </div>
-      ))}
-    </div>
-  );
+  return response.data.items;
 };
 
-export default SubPage;
+export const VideoList = () => {
+  const { isLoading, error, data } = useQuery('videos', () => fetchVideos('자취생 레시피'));
+
+  if (isLoading) return 'Loading...';
+
+  if (error) return `An error has occurred: ${error.message}`;
+
+  return (
+    <ul>
+      {data.map((video) => (
+        <li key={video.id.videoId}>
+          <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+          {video.snippet.title} <div>게시일 : {video.snippet.publishedAt}</div>
+          <div>작성자: {video.snippet.channelTitle}</div>
+        </li>
+      ))}
+    </ul>
+  );
+};
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,13 +1,19 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SubPage from '../components/SubPage';
+import { VideoList } from '../components/SubPage';
+import { fetchVideos } from '../components/SubPage';
+const queryClient = new QueryClient(); // QueryClient 인스턴스 생성
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SubPage />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<fetchVideos />} /> */}
+          <Route path="/" element={<VideoList />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
