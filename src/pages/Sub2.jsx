4909d20f749c809d61b41b2504563454e36db7c3
이예지 <nana@yjM.local>
2023-07-20 17:52:00 +0900

    Sub2 병합

--- src/pages/Sub2.jsx
+++ src/pages/Sub2.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import VideoSec from '../components/detail/VideoSec';
 import CommentInput from '../components/detail/CommentInput';
